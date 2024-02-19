1e2c7459c4018355e9cd4de675dba620fd613ebc
Vlads <87931410+Vladislayyyyyy@users.noreply.github.com>
2024-02-19 21:32:10 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+using namespace std;
+int main(){
+    int first;
+    int second;
+    int largest_common_divisor;
+    int c;
+    cout << "Enter first number:" << endl;
+    cin >> first;
+    cout << "Enter second number" << endl;
+    cin >> second;
+    if (first > second){
+        largest_common_divisor = first;
+    }
+    else{
+        largest_common_divisor = second;
+    }
+    for (int i = 1; i<=largest_common_divisor; i++){
+        if (first%i == 0 and second%i==0){
+           c = i;
+        }
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+    }
+    cout << "The greatest common divisor equals: " << c << endl;
+
+return 0;
+
+    }
